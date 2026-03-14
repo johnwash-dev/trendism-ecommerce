62bb23ff660756b45f0fb5097437ebc951326d94
johnwash2323 <johnwash313@gmail.com>
2026-03-14 19:52:28 +0530

    Fix issues

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -224,7 +224,6 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-    'MEDIA_TAG': 'media',
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
