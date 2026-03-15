24f8230590768a57f51089c03dac20e32afc3b2f
johnwash2323 <johnwash313@gmail.com>
2026-03-15 14:56:35 +0530

    Fixed email login

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -194,8 +194,9 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'pureheart70156@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'pureheart70156@gmail.com'
