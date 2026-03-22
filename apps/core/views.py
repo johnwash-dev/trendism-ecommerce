3dcd3b828e76744e4ce7635a50282e22755a9c4c
johnwash2323 <johnwash313@gmail.com>
2026-03-22 23:24:56 +0530

    added a Eid collection banner

--- apps/core/views.py
+++ apps/core/views.py
@@ -3,11 +3,24 @@ from ..products.models import Category,Product, HomeBanners
 
 # Create your views here.
 def homePage(request):
+    
 
     for p in Product.objects.all():
        p.save()
     banners = HomeBanners.objects.filter(is_active=True)
     trending_categories = Category.objects.filter(is_trending=True).order_by('order')[:8]
 
+    top_deals = Category.objects.filter(top_deals=True)[:10]
+
+    eid_banner = HomeBanners.objects.filter(title__icontains='Eid', is_active=False).first()
+
+    context ={
+        'banners' : banners,
+        'featured_product' : featured_product,
+        'trending_categories' : trending_categories,
+        'top_deals' : top_deals,
+        'eid_banner' : eid_banner,
+    }
+
     featured_product = Product.objects.filter(is_featured=True)[:8]
-    return render(request, 'home.html', {'banners' : banners, 'featured_product' : featured_product, 'trending_categories':trending_categories})
+    return render(request, 'home.html', context)
