f5ac5065c200a6ac1c109293ce28614a5bf259de
johnwash2323 <johnwash313@gmail.com>
2026-03-23 21:55:42 +0530

    fixing urls

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category/<slug:category_slug>/', views.product_list, name='product_list'),
+    path('', views.product_list, name='product_list'),
     path('<slug:slug>/', views.product_detail, name='product_detail'),
 ]
